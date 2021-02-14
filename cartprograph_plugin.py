d9037fd1832981e8e1d216a4df0fd05a85996a3c
Connor Nelson <Connor@ConnorNelson.com>
2021-02-14 01:39:36 -0700

    Refactor CartprographPlugin

--- cartprograph_plugin.py
+++ cartprograph_plugin.py
@@ -1,21 +1,17 @@
-from types import SimpleNamespace
+import types
+
 import networkx as nx
-import itertools
-import string
-import random
+
 from angrmanagement.plugins import BasePlugin
-from PySide2.QtWidgets import QMessageBox
+
 from .cartprograph_view import CartprographView
 from .cartprograph_client import CartprographClient
 
 
 class CartprographPlugin(BasePlugin):
-    # adds menu options to the Plugin dropdown
-    MENU_BUTTONS = ["Open Messagebox"]
-
     def __init__(self, workspace):
         super().__init__(workspace)
-        workspace.cartprograph = SimpleNamespace()
+
         self.cartprograph_view = CartprographView(workspace, "center")
         workspace.default_tabs += [self.cartprograph_view]
         workspace.add_view(
@@ -24,6 +20,7 @@ class CartprographPlugin(BasePlugin):
             self.cartprograph_view.category,
         )
 
+        workspace.cartprograph = types.SimpleNamespace()
         workspace.cartprograph.graph = nx.DiGraph()
         workspace.cartprograph.client = CartprographClient(
             workspace,
@@ -32,17 +29,3 @@ class CartprographPlugin(BasePlugin):
                 workspace.cartprograph.graph
             ),
         )
-
-    # handles the MENU_OPTIONS
-    def handle_click_menu(self, idx):
-        if idx < 0 or idx >= len(self.MENU_BUTTONS):
-            return
-
-        # not sure why this is here
-        if self.workspace.instance.project is None:
-            print("WARN: Project is None.")
-
-        if idx == 0:  # bad, fix later.
-            QMessageBox.critical(
-                self.workspace._main_window, "Test!", "This is a test. You caused this."
-            )
