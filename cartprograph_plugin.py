de1271d231af05d00d1dd6129f5408a6b31dbf0b
Connor Nelson <Connor@ConnorNelson.com>
2021-03-04 17:21:46 -0700

    Support instruction filter

--- cartprograph_plugin.py
+++ cartprograph_plugin.py
@@ -12,6 +12,7 @@ class CartprographPlugin(BasePlugin):
     def __init__(self, workspace):
         super().__init__(workspace)
 
+        self.workspace = workspace
         workspace.cartprograph = types.SimpleNamespace()
 
         self.cartprograph_view = CartprographView(workspace, "center")
@@ -29,4 +30,14 @@ class CartprographPlugin(BasePlugin):
             lambda node_id: self.cartprograph_view.update_graph(
                 workspace.cartprograph.graph
             ),
+        )
+
+        workspace.add_disasm_insn_ctx_menu_entry(
+            "Cartprograph &filter", self.handle_filter, add_separator_first=True
+        )
+
+    def handle_filter(self, context_menu):
+        insn_addr = context_menu.insn_addr
+        self.workspace.cartprograph.apply_filter(
+            lambda trace: insn_addr in trace["basic_blocks"]
         )
